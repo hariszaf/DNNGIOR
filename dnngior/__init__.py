5feba184755ff56933f3a59d63246c7d3078a624
Haris Zafeiropoulos <haris.zafeiropoulos@kuleuven.be>
2024-11-25 17:16:11 +0100

    update biochemistry

--- dnngior/__init__.py
+++ dnngior/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.0.3"
 
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tarfile
 from pathlib import Path
 
@@ -11,13 +11,13 @@ BiochemRoot = os.path.join(path, 'files', 'biochemistry')
 rxns_file = "reactions.tsv"
 ff = Path(os.path.join(BiochemRoot, rxns_file))
 if not ff.is_file():
-    file = tarfile.open(os.path.join(BiochemRoot, 'reactions.tsv.tar.gz'))
+    file = tarfile.open(os.path.join(BiochemRoot, 'reactions.tar.gz'))
     file.extractall(BiochemRoot)
 
 cpds_file = "compounds.tsv"
 ff = Path(os.path.join(BiochemRoot, cpds_file))
 if not ff.is_file():
-    file = tarfile.open(os.path.join(BiochemRoot, 'compounds.tsv.tar.gz'))
+    file = tarfile.open(os.path.join(BiochemRoot, 'compounds.tar.gz'))
     file.extractall(BiochemRoot)
 
 import dnngior.files
@@ -28,7 +28,7 @@ from dnngior.MSEED_compounds import Compounds
 from dnngior.MSEED_reactions import Reactions
 from dnngior.reaction_class  import Reaction
 
-import dnngior.addExchanges 
+import dnngior.addExchanges
 
 from dnngior.gapfill_class import Gapfill
 
